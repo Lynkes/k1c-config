8f1c7d9f17dcff4ef8fff089b7526db4f3b7c3ff
Development Agent <dev@vet-laudo.local>
2026-05-21 16:31:41 -0300

    configs, source code form creality branch klipper(prtouch_v1,2)

---
+++ config/Untitled-1.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+KLIPPER_PATH="/usr/share/klipper"
+AUTOTUNETMC_PATH="/usr/data/printer_data/config/klipper_tmc_autotune"
+
+if [[ -e ${KLIPPER_PATH}/klippy/plugins/ ]]; then
+    KLIPPER_PLUGINS_PATH="${KLIPPER_PATH}/klippy/plugins/"
+else
+    KLIPPER_PLUGINS_PATH="${KLIPPER_PATH}/klippy/extras/"
+fi
+
+set -eu
+export LC_ALL=C
+
+function preflight_checks {
+    # Try to determine the klippy virtual environment from the local Moonraker instance
+    KLIPPY_PYTHON_PATH=$(wget -qO- http://localhost:7125/printer/info | python -c 'import sys, json; print(json.load(sys.stdin)["result"]["python_path"])' 2>/dev/null || true)
+    # Fall back to the default location
+    KLIPPY_PYTHON_PATH=${KLIPPY_PYTHON_PATH:-"/usr/share/klippy-env/bin/python"}
+    # Get the major Python version
+    KLIPPY_PYTHON_VERSION=$("${KLIPPY_PYTHON_PATH}" -c 'import sys; print(sys.version_info.major)')
+
+    if [[ ${KLIPPY_PYTHON_VERSION} -lt 3 ]]; then
+        echo "[ERROR] Klipper must be using Python 3 - detected outdated Python 2"
+        exit 1
+    else
+        echo "[PRE-CHECK] Klipper is using Python 3!"
+    fi
+
+    printf "\n\n"
+}
+
+function check_download {
+    local autotunedirname autotunebasename
+    autotunedirname="$(dirname "${AUTOTUNETMC_PATH}")"
+    autotunebasename="$(basename "${AUTOTUNETMC_PATH}")"
+
+    if [ ! -d "${AUTOTUNETMC_PATH}" ]; then
+        echo "[DOWNLOAD] Downloading Autotune TMC repository..."
+        if git -C "${autotunedirname}" clone https://github.com/andrewmcgr/klipper_tmc_autotune.git $autotunebasename; then
+            chmod +x "${AUTOTUNETMC_PATH}"/install.sh
+            printf "[DOWNLOAD] Download complete!\n\n"
+        else
+            echo "[ERROR] Download of Autotune TMC git repository failed!"
+            exit 1
+        fi
+    else
+        printf "[DOWNLOAD] Autotune TMC repository already found locally. Continuing...\n\n"
+    fi
+}
+
+function link_extension {
+    echo "[INSTALL] Linking extension to Klipper..."
+
+    ln -srfn "${AUTOTUNETMC_PATH}/autotune_tmc.py" "${KLIPPER_PLUGINS_PATH}/autotune_tmc.py"
+    ln -srfn "${AUTOTUNETMC_PATH}/motor_constants.py" "${KLIPPER_PLUGINS_PATH}/motor_constants.py"
+    ln -srfn "${AUTOTUNETMC_PATH}/motor_database.cfg" "${KLIPPER_PLUGINS_PATH}/motor_database.cfg"
+}
+
+function restart_klipper {
+    echo "[POST-INSTALL] Restarting Klipper..."
+    sudo systemctl restart klipper
+}
+
+
+printf "\n======================================\n"
+echo "- Autotune TMC install script -"
+printf "======================================\n\n"
+
+
+# Run steps
+preflight_checks
+check_download
+link_extension
+restart_klipper
